486dc9cb487802c9f00001c511e02573c3c44cb2
Suman Mishra <jedsdofficial@gmail.com>
2025-01-09 16:47:22 +0530

    loader change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Loader from '@/components/Genaral/Loader';
 import Footer from "@/components/home/Footer";
 import HomeFirstElement from "@/components/home/HomeFirstElement";
@@ -11,17 +11,17 @@ import SmNavbar from "@/components/home/SmNavbar";
 import UploadProcedure from "@/components/home/UploadProcedure";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 3000);
+  // }, []);
 
 
   return (
-    loading ? <Loader /> : (
+    // loading ? <Loader /> : (
         <div className="bg-blue-50 h-screen overflow-x-hidden w-screen overflow-auto no-scrollbar">
         <div className="NavItems w-full h-fit hidden xl:block fixed z-[555]">
           <Navbar />
@@ -35,6 +35,6 @@ export default function Home() {
         <UploadProcedure />
         <Footer />
       </div>
-      )
+      
   );
 }
