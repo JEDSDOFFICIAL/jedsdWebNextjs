a049b32ee53195a04790028d6a66576c518edb7b
Suman Mishra <jedsdofficial@gmail.com>
2025-01-13 12:23:35 +0530

    fixed 2

--- src/components/User/app-sider.tsx
+++ src/components/User/app-sider.tsx
@@ -46,8 +46,30 @@ const items = [
 
 export function AppSidebar() {
   return (
-    <Sidebar>
-      <SidebarContent />
-    </Sidebar>
+    <Sidebar >
+    <SidebarHeader>
+<SidebarMenu>
+ <SidebarMenuItem>
+   <DropdownMenu>
+     <DropdownMenuTrigger asChild>
+       <SidebarMenuButton>
+         Select Workspace
+         <ChevronDown className="ml-auto" />
+       </SidebarMenuButton>
+     </DropdownMenuTrigger>
+     <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+       <DropdownMenuItem>
+         <span>Acme Inc</span>
+       </DropdownMenuItem>
+       <DropdownMenuItem>
+         <span>Acme Corp.</span>
+       </DropdownMenuItem>
+     </DropdownMenuContent>
+   </DropdownMenu>
+ </SidebarMenuItem>
+</SidebarMenu>
+</SidebarHeader>
+<SidebarContent />
+</Sidebar>
   )
 }
